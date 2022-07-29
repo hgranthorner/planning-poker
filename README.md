c982321047b5551587ee32e48fd773c6b3f1ffb0
Grant Horner <h.grant.horner@gmail.com>
2022-07-29 18:01:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Poker Planning
+
+## Getting Started
+Copy `src/credentials.ts.example` -> `src/credentials.ts` and replace the values.
+`npm install`
+`npm run dev`
---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {createClient} from '@supabase/supabase-js'
+    import {env} from './credentials'
+
+    const supabase = createClient(env.apiUrl, env.anon)
+</script>
+<style></style>
+{#await supabase.from('players').select('*').then(({data, error}) => {
+    if (error) throw error
+    return data
+})}
+    fetching...
+{:then data}
+    {JSON.stringify(data)}
+{/await}
